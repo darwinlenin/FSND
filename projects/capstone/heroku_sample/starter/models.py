aa203f7dfe188cdfc712ca8ddcd035d5f54c5457
darwinlenin <darwinlenin@gmail.com>
2020-05-31 10:28:56 -0500

    heroku configuration and publish app

--- projects/capstone/heroku_sample/starter/models.py
+++ projects/capstone/heroku_sample/starter/models.py
@@ -1,6 +1,8 @@
+import os
 from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from sqlalchemy.types import Integer
 
 database_path = os.environ['DATABASE_URL']
 
---
+++ projects/capstone/heroku_sample/starter/setup.sh
@@ -0,0 +1,2 @@
+EXCITED=true
+DATABASE_URL=postgresql://capstone:capstone@localhost:5432/capstone
